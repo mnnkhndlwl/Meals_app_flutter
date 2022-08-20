9474f4a7e05318a635f2b0382be0563a46d1c80a
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-08-21 00:54:51 +0530

    meal_item completed

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
 import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
@@ -21,9 +22,17 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: ListView.builder(itemBuilder: (ctx,index){
-          return Text(categoryMeals[index].title);
-      },itemCount: categoryMeals.length,),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }

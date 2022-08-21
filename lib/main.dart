313078e8c03c8b5db67ac16caebacb0edeee60de
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-08-22 01:19:34 +0530

    added bottom tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.deepOrangeAccent,
-        canvasColor: Colors.pinkAccent,
+         primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
               bodyText1: TextStyle(
-                color: Color.fromARGB(255, 214, 194, 194),
+                color: Color.fromRGBO(20, 51, 51, 1),
               ),
               bodyText2: TextStyle(
-                color: Color.fromARGB(255, 181, 150, 150),
+                color: Color.fromRGBO(20, 51, 51, 1),
               ),
               headline6: TextStyle(
                 fontFamily: 'RobotoCondensed',
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
               ),
             ),
       ),
